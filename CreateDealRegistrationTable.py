22d163b616d747fdc0a4fcf35b418ff7333ed71f
Kate Harbison <kharbison9319@gmail.com>
2019-12-07 16:52:01 -0500

    Add row class and column verification for each row

--- CreateDealRegistrationTable.py
+++ CreateDealRegistrationTable.py
@@ -2,11 +2,12 @@ import os
 import sys
 import argparse
 import csv
-import datetime
-from dateutil.parser import parse
+import re
+from datetime import datetime
+import DealRegRow
 
 #region Variables
-
+RowsToAdd = []
 #endregion
 
 #region Define Arg Parser
@@ -18,28 +19,81 @@ def parse_args():
 #endregion
 
 #region Funtions
-def parseDate(dateToParse):
-  try:
-    dt = parse(dateToParse)
-    return dt
-  except:
-    dt = datetime.datetime.strptime(dateToParse, "%Y-%m-%d-%H.%M.%S.%f")
-    return dt
+def verifyPNFormat(partNumber):
+  result = re.fullmatch("^[A-Z]+PT[0-9]+$", partNumber)
+  if result == None:
+    raise ValueError()
 
-def parseRow(rowToParse):
-  parsedRow = dict.copy(rowToParse)
-  for column, value in parsedRow.items():
-    if 'DATE' in column:
-      # Abreviated year 99 from 9999 will be interpreted as 1999 instead of 9999.
-      # Verify that Start date comes after 1999 to ensure 9999 would be correct date.
-      if column == 'END_DATE' and '99' in value and not '9999' in value:
-        if type(parsedRow['START_DATE']) is datetime.datetime and parsedRow["START_DATE"].year > 1999:
-          value = str.replace(value, '99', '9999')
-        # Todo: Handle instance when start time as not been parsed yet. Will not happen with current csv file format.
+def verifyDelRegGroup(regGroup):
+  if regGroup == '':
+    raise ValueError()
+
+def parseDate(dateToParse):
+  for dtFmt in ("%m/%d/%Y %I:%M:%S %p","%m/%d/%y %I:%M:%S %p", "%Y-%m-%d-%H.%M.%S.%f", "%m/%d/%y", "%m/%d/%Y"):
+    try:
+      return datetime.strptime(dateToParse, dtFmt)
+    except ValueError:
+      pass
+
+  raise ValueError()
+
+def parseActiveFlag(activeFlag):
+  flag = activeFlag.upper()
+  if not flag == 'Y' and not flag == 'N':
+    raise ValueError()
+  else:
+    return flag
+
+def verifyAndParseRow(rowToParse):
+  parsedRow = {}
+  #verify each column value for the given row
+  for column, value in rowToParse.items():
+    columnName = column.upper()
+
+    if columnName == 'PART_NUM':
+      # Verify part number starts with prefix and has PT# afterwards
       try:
-        parsedRow[column] = parseDate(value)
+        verifyPNFormat(value)
+        parsedRow[columnName] = value
+        print(f'{column} is valid')
       except ValueError:
-        raise(ValueError(f'Unable to parse date into dateTime value: {value}'))
+        raise ValueError(f'PartNumber does not fit the specified format of *PT#: {value}')
+
+    elif columnName == 'DEAL_REG_GROUP':
+      try:
+        verifyDelRegGroup(value)
+        parsedRow[columnName] = value
+        print(f'{column} is valid')
+      except ValueError:
+        raise ValueError(f'Deal Registration Group cannot be empty.')
+
+    elif 'DATE' in columnName:
+      if 'END' in columnName:
+        # Abreviated year 99 from 9999 will be interpreted as 1999 instead of 9999.
+        if columnName == 'END_DATE' and '99' in value and not '9999' in value:
+          if 'ACTIVE_FLAG' in parsedRow:
+            if parsedRow['ACTIVE_FLAG'] == 'Y':
+              value = str.replace(value, '99', '9999')
+          else:
+              value = str.replace(value, '99', '9999')
+
+      # Attempt Date Time Parse
+      try:
+        parsedRow[columnName] = parseDate(value)
+        print(f'{column} is valid and parsed to {parsedRow[columnName]}')
+      except ValueError:
+        raise ValueError(f'Unable to parse {column} into dateTime value: {value}')
+
+    elif columnName == "ACTIVE_FLAG":
+      try:
+        parsedRow[columnName] = parseActiveFlag(value)
+        print(f'{column} is valid and parsed to {parsedRow[columnName]}')
+      except ValueError:
+        raise ValueError(f'Active Flag value is not in the correct format of Y or N: {value}')
+
+    else:
+      pass
+
   return parsedRow
 
 
@@ -55,10 +109,8 @@ for fileName in args.csvFiles:
     reader = csv.DictReader(csvfile)
     index = 0
     for row in reader:
-      parsedRow = parseRow(row)
+      parsedRow = verifyAndParseRow(row)
       print(parsedRow)
-      print(parsedRow['START_DATE'])
-      print(parsedRow['END_DATE'])
       index = index + 1
       #Don't want to go through all lines in file right now so break after 10 lines
       if index > 9 :
